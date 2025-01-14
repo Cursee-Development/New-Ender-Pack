36e65e19cb58a99ec98bc6245efad788dfe90fbb
jason13official <jason13official@gmail.com>
2025-01-14 18:52:18 -0500

    fix mod version in Constants

--- common/src/main/java/com/cursee/ender_pack/Constants.java
+++ common/src/main/java/com/cursee/ender_pack/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
 
 	public static final String MOD_ID = "ender_pack";
 	public static final String MOD_NAME = "EnderPack";
-	public static final String MOD_VERSION = "1.0.0";
-	public static final String MC_VERSION_RAW = "[1.20.1]";
+	public static final String MOD_VERSION = "2.1.0";
+	public static final String MC_VERSION_RAW = "[1.21.1]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
